c782b60db9ab52f9071cd384715e43550275588e
jieni666 <1035569251@qq.com>
2019-03-07 21:26:59 +0800

    aaa

---
+++ multiplicationTable.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()	
+{
+	int n=9;
+	int i,j;
+	while (i<=n){
+		j=1;
+		while(j<=i){
+			printf("%d*%d=%d",j,i,i*j);
+			if(i*j<10){
+				printf("   ");
+			}
+			else{
+				printf("  ");
+			}
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	return 0;
+}
+ 
+
