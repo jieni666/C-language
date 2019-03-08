e6cf60bcbc9a5d161653488434c5158999dbd0ef
jieni666 <1035569251@qq.com>
2019-03-08 21:30:31 +0800

    zzz

---
+++ ox.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()	
+{
+	/**int i,j;
+	int numsSize=4;
+	int nums[] = {2, 7, 11, 15}, target = 9; 
+	for (i=0; i <numsSize - 1; i++)
+	{
+		for (j = i+1; j < numsSize; j++)
+		{
+			if (nums[i] + nums[j] == target)
+			{
+				printf("%d",nums[i]);
+				printf("%d",nums[j]);
+				printf("%d",target);
+	
+			}
+		}
+	}
+	return 0;**/
+	int i,j;
+	int numsSize=4;
+	int nums[] = {2,7,11,15}, target = 9; 
+	for (i=0; i <numsSize - 1; i++)
+	{
+		for (j = i+1; j < numsSize; j++)
+		{
+			if (nums[i] + nums[j] == target)
+			{
+			
+				printf("%d",i);
+				printf("%d",j);
+				printf("%d",target);
+	
+			}
+		}
+	}
+	return 0;
+	//return (sizeof(array) / sizeof(array[0]));
+}
+ 
+
