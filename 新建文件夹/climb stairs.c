3564fc6d6d1d4a71214da94a42f711ecbfe66a5e
jieni666 <1035569251@qq.com>
2019-03-09 23:08:55 +0800

    z

---
+++ 新建文件夹/climb stairs.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()	
+{
+	int n;
+    int sum=0,p2=2,p3=3;
+    scanf("%d",&n);
+    if(1==n){
+    	printf("有1种方式");
+    	return n;
+	}
+	
+    if(2==n){
+    	printf("有2种方式");
+    	return n;
+	} 
+    if(3==n){
+    	printf("有3种方式");
+    	return n;
+	} 
+    while(n>3)
+    {
+    	sum=p2+p3;
+    	p2=p3;
+    	p3=sum;
+    	n--;
+    }
+      printf("有%d种方式",sum);
+      return sum;
+
+}
