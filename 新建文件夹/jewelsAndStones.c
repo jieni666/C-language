d18a1e1c0651f3c40f8870299959003fe2278dbb
jieni666 <42937259+jieni666@users.noreply.github.com>
2019-04-08 20:56:33 +0800

    宝石与石头

---
+++ 新建文件夹/jewelsAndStones.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int numJewelsInStones(char* J, char* S);
+int main(){
+	char* a,*b;
+	 a= "zxc";
+	 b = "aAAbbbbzzzxxxccc";
+	numJewelsInStones(a, b);
+} 
+int numJewelsInStones(char* J, char* S) {
+    int count = 0;
+    int i,j;
+    for(i = 0;i < strlen(J);i++)
+        for(j = 0;j < strlen(S);j++)
+            if(J[i] == S[j])
+                count++;
+    return count;
+}
+//给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥
+//有的石头的类型，你想知道你拥有的石头中有多少是宝石。J 中的字母不重复，J 和 S中的所有字符
+//都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+//示例 1:
+//输入: J = "aA", S = "aAAbbbb"
+//输出: 3
+//示例 2:
+//输入: J = "z", S = "ZZ"
+//输出: 0
